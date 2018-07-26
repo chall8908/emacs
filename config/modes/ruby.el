d3665efac1346e0f6658285e0711443da5933ec7
Chris Hall <chall8908@gmail.com>
2018-07-26 17:35:50 -0500

    Cleanup robe and rubocop

--- config/modes/ruby.el
+++ config/modes/ruby.el
@@ -20,39 +20,11 @@
   :hook (enh-ruby-mode . ruby-electric-mode)
   )
 
-;; (use-package rubocop
-;;   :hook enh-ruby-mode
-;;   )
-
-;; (use-package ruby-tools
-;;   :diminish
-;;   :hook enh-ruby-mode
-;;   )
-
 (use-package rspec-mode
   :config
   (setq rspec-use-rvm t)
   (rspec-install-snippets)
   )
-
-;; robe is a bit overkill for me...
-
-;; (use-package inf-ruby
-;;   :after rvm
-;;   )
-
-;; (use-package robe
-;;   :after (enh-ruby-mode company)
-;;   :diminish
-;;   :hook enh-ruby-mode
-;;   :config
-;;   (push 'company-robe company-backends)
-;;   (defadvice inf-ruby-console-auto
-;;       (before activate-rvm-for-robe activate)
-;;     (rvm-activate-corresponding-ruby)
-;;     )
-;;   (add-hook 'robe-mode-hook 'robe-start)
-;;   )
 
 (provide 'ruby)
 ;;; ruby.el ends here
